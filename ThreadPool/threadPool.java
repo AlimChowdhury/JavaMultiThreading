7a3753f046ead1752ae350d5791e66c35cc1522c
AlimChowdhury <md.alim.chowdhury8@gmail.com>
2024-01-01 12:58:35 +0600

    Thread Pool

---
+++ ThreadPool/threadPool.java
@@ -0,0 +1,61 @@
+package ThreadPool;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+class workerThread implements Runnable{
+    private String message;
+   public workerThread(String message){
+        this.message = message;
+    }
+    private void processMessage() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName() + " (Start) message = " + message);
+        processMessage();//call processMessage method that sleeps the thread for 2 seconds
+        System.out.println(Thread.currentThread().getName() + " (End)");//prints thread name
+    }
+}
+
+
+/**
+ * Java Thread pool represents a group of worker threads that are waiting for the job and reused many times.
+ *
+ * In the case of a thread pool, a group of fixed-size threads is created. A thread from the thread pool is
+ * pulled out and assigned a job by the service provider. After completion of the job, the thread is contained
+ * in the thread pool again.
+ */
+
+public class threadPool {
+    public static void main(String[] args){
+
+/**
+ * creating a pool of 5 threads
+ */
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 1; i <= 10; i++) {
+            Runnable worker = new workerThread("" + i);
+            executor.execute(worker);//calling execute method of ExecutorService
+        }
+        executor.shutdown();
+
+        while (!executor.isTerminated()) {}
+
+        System.out.println("Finished all threads");
+    }
+}
+
+/**
+ * Explanation:
+ *
+ * when one wants to execute 50 tasks but is not willing to create 50
+ * threads. In such a case, one can create a pool of 10 threads. Thus, 10 out of 50 tasks are assigned, and the
+ * rest are put in the queue. Whenever any thread out of 10 threads becomes idle, it picks up the 11th task.
+ * The other pending tasks are treated the same way.
+ */
---
+++ ThreadPool/threadPool2.java
@@ -0,0 +1,77 @@
+package ThreadPool;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class tasks implements Runnable{
+    private String taskName;
+    public tasks(String taskName){
+        this.taskName =taskName;
+    }
+
+    @Override
+    public void run(){
+
+        try {
+            for (int j = 0; j < 3; j++) {
+                Date date = new Date();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh : mm : ss");
+                if (j == 0) {
+/**
+ * prints the initialization time for every task
+ */
+                    System.out.println("Initialization time for the task name: "+ taskName +
+                            " = " + simpleDateFormat.format(date));
+                }
+                else{
+/**
+ * prints the execution time for every task
+ */
+                    System.out.println("Time of execution for the task name: " + taskName +
+                            " = " +simpleDateFormat.format(date));
+                }
+                Thread.sleep(1000);
+            }
+            System.out.println(taskName + " is complete.");
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class threadPool2 {
+    static final int MAX_TH = 3;
+    public static void main(String[] args){
+/**
+ * Creating five new tasks
+ */
+        Runnable rb1 = new tasks("task 1");
+        Runnable rb2 = new tasks("task 2");
+        Runnable rb3 = new tasks("task 3");
+        Runnable rb4 = new tasks("task 4");
+        Runnable rb5 = new tasks("task 5");
+
+/**
+ * creating a thread pool with MAX_TH number of
+ * threads size the pool size is fixed
+ */
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_TH);
+
+        executor.execute(rb1);
+        executor.execute(rb2);
+        executor.execute(rb3);
+        executor.execute(rb4);
+        executor.execute(rb5);
+/**
+ * pool is shutdown
+ */
+        executor.shutdown();
+    }
+}
+
+/**
+ * Explanation: It is evident by looking at the output of the program that tasks 4 and 5 are executed only when
+ * the thread has an idle thread. Until then, the extra tasks are put in the queue.
+ */
